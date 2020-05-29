8bb73a413f9b843f33272d47e001e9c162523cb4
Léon Imanuel <leondino0309@gmail.com>
2020-05-29 15:01:02 +0200

    - Added all navigation components

--- app/src/main/java/com/example/quizmake/ui/MainActivity.kt
+++ app/src/main/java/com/example/quizmake/ui/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.quizmake.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.quizmake.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initNavigation()
     }
+
+    private fun initNavigation(){
+        val navController =  findNavController(R.id.navHostFragment)
+
+        val appBarrConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarrConfiguration)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                navController.navigateUp()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
